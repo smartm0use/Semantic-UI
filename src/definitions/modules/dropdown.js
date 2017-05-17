a955eb92c3a915af7c2a9c3a30959c6c41180edd
fabsilib <fabio.siliberto@eng.it>
2017-05-17 18:00:47 +0200

    Force selection now select previously selected items if there are some

--- src/definitions/modules/dropdown.js
+++ src/definitions/modules/dropdown.js
@@ -874,9 +874,7 @@ $.fn.dropdown = function(parameters) {
           var
             $currentlySelected = $item.not(className.filtered).filter('.' + className.selected).eq(0),
             $activeItem        = $item.not(className.filtered).filter('.' + className.active).eq(0),
-            $selectedItem      = ($currentlySelected.length > 0)
-              ? $currentlySelected
-              : $activeItem,
+            $selectedItem      = $activeItem,
             hasSelected = ($selectedItem.length > 0)
           ;
           if(hasSelected) {
